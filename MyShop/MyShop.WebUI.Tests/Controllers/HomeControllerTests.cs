2a0649f2c55f07e3ffc4415c0658a1185be1c1da
WeConnectIT <info@weconnect-it.nl>
2022-03-28 10:39:25 +0200

    BasketTests

---
+++ MyShop/MyShop.WebUI.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyShop.WebUI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyShop.WebUI.Controllers;
+using MyShop.Core.Models;
+using MyShop.Core.Contracts;
+using MyShop.Core.ViewModels;
+
+namespace MyShop.WebUI.Tests.Controllers
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IndexPageDoesReturnProducts()
+        {
+            IRepository<Product> productContext = new Mocks.MockContext<Product>();
+            IRepository<ProductCategory> productCategoryContext = new Mocks.MockContext<ProductCategory>();
+
+            productContext.Insert(new Product());
+            HomeController controller = new HomeController(productContext, productCategoryContext);
+
+            var result = controller.Index() as ViewResult;
+            var viewmodel = (ProductListViewModel)result.ViewData.Model;
+
+            Assert.AreEqual(1, viewmodel.Products.Count());
+
+
+        }
+
+      
+    }
+}
